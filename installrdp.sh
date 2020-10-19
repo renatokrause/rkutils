a1b349879f966347865bde2727f5eb82cd0759a8
Renato Krause <renatokrause@gmail.com>
2020-10-19 11:59:39 -0300

    Update installrdp.sh

--- installrdp.sh
+++ installrdp.sh
@@ -1,5 +1,6 @@
 apt install xrdp
 sed -i 's/port=vsock/port=tcp/' /etc/xrdp/xrdp.ini
 sed -i 's/-1:3389/:3389/' /etc/xrdp/xrdp.ini
+mkdir -p /etc/xrd/
 echo "sleep 2s" >> /etc/xrd/reconnectwm.sh
 echo "setxkbmap -model abnt2 -layout br" >> /etc/xrd/reconnectwm.sh
